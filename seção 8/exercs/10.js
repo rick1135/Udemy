1b2e321536fe8369ae0ba19d0024811f15ccb1e5
rick1135 <rick1135@hotmail.com>
2023-05-23 15:30:04 -0300

    .

--- seção 8/exercs/10.js
+++ seção 8/exercs/10.js
@@ -1,1 +1,10 @@
-function 
+function receberPrimeiroEUltimoElemento(elementos) {
+    const indiceDoPrimeiroElemento = 0;
+    const indiceDoUltimoElemento = elementos.length - 1;
+    const primeiroElemento = elementos[indiceDoPrimeiroElemento];
+    const ultimoElemento = elementos[indiceDoUltimoElemento];
+    return [primeiroElemento, ultimoElemento];
+}
+
+console.log(receberPrimeiroEUltimoElemento([7, 14, "olá"]));
+console.log(receberPrimeiroEUltimoElemento([-100, "aplicativo", 16]));
