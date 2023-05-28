a3f79a0c71f7933599e08694eaa6d45d6f367e6c
rick1135 <rick1135@hotmail.com>
2023-05-28 20:52:46 -0300

    studying 05/28/2023

---
+++ seção 10/middlewares.js
@@ -0,0 +1,25 @@
+// padrão middleware pattern (chain of responsability)
+const passo1 = (ctx, next) => {
+    ctx.valor1 = 'mid1';
+    next();
+}
+
+const passo2 = (ctx, next) => {
+    ctx.valor2 = 'mid2';
+    next();
+}
+
+const passo3 = ctx => ctx.valor3 = 'mid3';
+
+// Função que recebe um array de funções (middlewares) e retorna uma função que recebe um contexto (ctx)
+// e executa os middlewares em sequência, passando o contexto para cada um deles.
+const exec = (ctx, ...middlewares) => {
+    const execPasso = indice => {
+        middlewares && indice < middlewares.length && middlewares[indice](ctx, () => execPasso(indice + 1));
+    }
+    execPasso(0);
+}
+
+const ctx = {};
+exec(ctx, passo1, passo2, passo3);
+console.log(ctx); // { valor1: 'mid1', valor2: 'mid2', valor3: 'mid3' }
