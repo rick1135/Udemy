4eb5a5d64808f8afb08dfc88c711f7cf2c3313a2
Alberto Henrique <rick1135@hotmail.com>
2024-01-28 18:06:44 -0300

    revisando

---
+++ Node/revisando/global.js
@@ -0,0 +1,8 @@
+// console.log(global);
+
+global.MinhaApp=Object.freeze({
+    saudacao(){
+        return 'Tô em todo lugar'
+    },
+    nome: 'Sistema daora'
+})
---
+++ Node/revisando/passandoParametrosCliente.js
@@ -0,0 +1,2 @@
+const saudacoes = require('./passandoParametros')('Ana', 'Lucas', 'Wast');
+console.log(saudacoes);
